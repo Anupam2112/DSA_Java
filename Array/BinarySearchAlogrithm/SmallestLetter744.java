4eecb610d545d8eb96e6336c6812b66802b5ef9d
Anupam2112 <mishraanupam603@gmail.com>
2026-09-02 22:17:56 +0530

    Added SmallestLetter744

---
+++ Array/BinarySearchAlogrithm/SmallestLetter744.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+public class SmallestLetter744 {
+
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+
+        char ch = SmallestLetter(letters, target);
+        System.out.println(ch);
+    }
+
+    static char SmallestLetter(char[] letters, char target) {
+      int st = 0;
+      int ed = letters.length-1;
+
+      while(st <= ed) {
+          int mid = st+(ed-st)/2;
+
+          if(target < letters[mid] ) {
+              ed = mid-1;
+          }else {
+              st = mid+1;
+          }
+      }
+      return letters[st%letters.length];
+    }
+}
