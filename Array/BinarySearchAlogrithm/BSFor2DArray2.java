3c7b3b41417f7a1a16557b58fe856ff8f1409872
Anupam2112 <mishraanupam603@gmail.com>
2026-09-13 00:31:55 +0530

    done

--- Array/BinarySearchAlogrithm/BSFor2DArray2.java
+++ Array/BinarySearchAlogrithm/BSFor2DArray2.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class BSfor2DArray2 {
+public class BSFor2DArray2 {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3, 4},
                       {5, 6, 7, 8},
